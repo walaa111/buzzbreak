181c7916fc605c00d8ac18cc26b726dae9a9843f
walaa111 <walaagamil18@gmail.com>
2020-03-16 15:51:24 +0200

    add

--- buzzbreak/www/js/index.js
+++ buzzbreak/www/js/index.js
@@ -125,5 +125,22 @@ var app = {
 
 
            //admob
+//notify
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
 
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("6bb1e8be-c6aa-43b2-9e96-9dfad497a41a")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
+//end notify
 app.initialize();
